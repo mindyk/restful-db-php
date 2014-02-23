57e3707d997d98345cffcc9798a87b5be8441479
mindyk <michael.indyk@googlemail.com>
2014-02-24 00:04:18 +0100

    molding

--- www/index.php
+++ www/index.php
@@ -6,10 +6,11 @@ require_once PATH_ROOT . 'vendor/autoload.php';
 
 //+++ CONFIG
 if (!is_readable(PATH_CONFIG)) {
-	throw new Exception("etc/config.json not readable. Did you forgot to roge it from etc/config.json-dist?");
+	throw new Exception("etc/config.json not readable. Did you forgot to forge it from etc/config.json-dist?");
 }
 $configRaw = file_get_contents(PATH_CONFIG);
-$config = json_encode($configRaw);
+$config = json_decode($configRaw);
+//wwwDebug($config);
 //--- CONFIG
 
 //+++ DB
@@ -18,9 +19,48 @@ $db = new mysqli($config->mysql->host, $config->mysql->user, $config->mysql->pwd
 
 //+++ APP
 $app = new \Slim\Slim();
+$app->get('/', function () {
+	echo '<h1> ROOT</h1>';
+});
 
-$app->get('/dbs/:dbName/tbls/:tblName/', function ($dbName, $tblName) use ($db) {
+$app->get($config->prefix, function () {
+	echo '<h1> RESTFUL DB API</h1>';
+});
+$app->get($config->prefix . '/dbs/:dbName/tbls/:tblName', function ($dbName, $tblName) use ($app, $db) {
+	$result = $db->query(sprintf("EXPLAIN %s.%s", $dbName, $tblName));
+	$resultAsArray = array();
+	while ($row = $result->fetch_assoc()) {
+		$resultAsArray[$row['Field']] = $row;
+	}
+	wwwDebug($resultAsArray);
+	$sort = $app->request->get('sort');
+	$fields = $app->request->get('fields');
+	$page = $app->request->get('page');
+	$perPage = $app->request->get('perPage');
 
+	$filters = array();
+
+	foreach ($resultAsArray as $key => $col) {
+		$filterValue = $app->request->get($key);
+		if (!empty($filterValue)) {
+			$filters[$key] = $filterValue;
+		}
+	}
+
+	$request = array(
+		'sort' => $sort,
+		'fields' => $fields,
+		'page'	=> $page,
+		'per_page' => $perPage,
+		'filters' => $filters,
+	);
+
+	wwwDebug($request);
 });
 $app->run();
 //--- APP
+
+function wwwDebug($value)
+{
+	echo '<pre>' . print_r($value, true) . '</pre>';
+}
